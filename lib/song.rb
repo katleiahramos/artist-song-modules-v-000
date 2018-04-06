7c0624188d170b7863b5e9ac435ce0e1895e7bc5
Katleiah Ramos <katleiahramos@gmail.com>
2018-04-06 15:15:06 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,8 +3,9 @@ require 'pry'
 class Song
   extend Memorable::ClassMethods
   include Memorable::InstanceMethods
+
   extend Findable
-   include Paramable
+  include Paramable
 
   attr_accessor :name
   attr_reader :artist
