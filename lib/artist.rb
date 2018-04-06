a64a05195d94d42ed76292715932779b2e5e3d95
Katleiah Ramos <katleiahramos@gmail.com>
2018-04-06 15:14:47 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -5,7 +5,7 @@ class Artist
   include Memorable::InstanceMethods
   # extend Memorable
   extend Findable
-  # include Paramable
+   include Paramable
 
   attr_accessor :name
   attr_reader :songs
